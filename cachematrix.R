76d6605300a92ade8d54b9fa7504256196c6c607
Costas <56643958+cpexos@users.noreply.github.com>
2019-12-08 10:21:51 -0500

    Update cachematrix.R
    R Programming Assignment #2 (Coursera)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a cache of the inverse of a given matrix
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) {  ## function call to cache the matrix x
+        inversion <- NULL                    ## asigns NULL to inversion
+        set <- function(y) {
+                x <<- y
+                inversion <<- NULL  ## function set inversion as NULL in global environment
+        }
+        get <- function() x ## function with no arguement, return x in the scope
+        setTheInversion <- function(inverse) inversion <<- solve(x) ##function calculate inverse of x, update insersion globally
+        getTheInversion <- function() inversion ##function return inversion
+        list(set = set, get = get, setTheInversion = setTheInversion, getTheInversion = getTheInversion)## returns a list of functions
 }
 
-
-## Write a short comment describing this function
+## This function returns the inverse of the matrix from the function above
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inversion <- x$getTheInversion()    ## assigns result from getTheInversion to inversion
+        if(!is.null(inversion)){            ## checks to see if inversion is set to NULL or not
+                return(inversion)           ## returns result of inversion if it is not NULL
+        }
+        matrixdata <- x$get()               ## matrixdata is the matrix arguement used in makeCacheMatrix
+        inversion <- solve(matrixdata, ...) ## calculate inverse of matrixdata, will be passed to cacheSolve as its arguement
+        x$setTheInversion(inversion)        ## assign inversion globally
+        inversion                           ## returns inversion                        
 }
